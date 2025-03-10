c72f6db2b325dce115b70403d2401fa7f1a7794d
serejkaka <film28728@gmail.com>
2025-03-10 14:08:42 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Простой скрипт для добавления кликабельных карточек
 document.querySelectorAll('.product-card a').forEach(link => {
   link.addEventListener('click', function(event) {
     window.location.href = link.getAttribute('href');
